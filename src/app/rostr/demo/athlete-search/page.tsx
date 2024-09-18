913e89b25dd1ed7e73036dcf6db96bea48778f8f
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2024-09-18 12:44:42 -0500

    suspense boundry

--- src/app/rostr/demo/athlete-search/page.tsx
+++ src/app/rostr/demo/athlete-search/page.tsx
@@ -6,19 +6,28 @@ import MatchingProfiles from '@/components/rostr/athlete-search/MatchingProfiles
 import PageNumbers from '@/components/rostr/athlete-search/PageNumbers';
 import { useSearchParams } from 'next/navigation';
 
-export default function AthleteSearchPage() {
+function Content() {
     const searchParams = useSearchParams();
-    const rostrId = searchParams.get('rostrId');    
+    const rostrId = searchParams.get('rostrId');
+
+    return (
+        <div>
+        <Filters defaultRostrId={rostrId ? rostrId : undefined} />
+            <div className='px-8'>
+                <MatchingProfiles />
+                <PageNumbers />
+            </div>
+        </div>
+    )
+}
+
+export default function AthleteSearchPage() { 
 
     return (
         <div className="flex flex-col min-h-full bg-black pt-16 pb-20 w-full">
                 {/* <h1 className="text-white text-center text-6xl font-title mb-8">ATHLETE SEARCH</h1> */}
                 <Suspense fallback={<div>Loading...</div>}>
-                    <Filters defaultRostrId={rostrId ? rostrId : undefined} />
-                    <div className='px-8'>
-                        <MatchingProfiles />
-                        <PageNumbers />
-                    </div>
+                    <Content/>
                 </Suspense>
         </div>
     );
