2f20fc2c270fe38b075895663642c239bcc3573c
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2024-09-18 12:37:39 -0500

    fixing suspense

--- src/app/rostr/demo/athlete-search/page.tsx
+++ src/app/rostr/demo/athlete-search/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-export const dynamic = 'force-dynamic';
-
+import React, { Suspense } from 'react';
 import Filters from '@/components/rostr/athlete-search/filters/Filters'
 import MatchingProfiles from '@/components/rostr/athlete-search/MatchingProfiles';
 import PageNumbers from '@/components/rostr/athlete-search/PageNumbers';
@@ -14,11 +13,13 @@ export default function AthleteSearchPage() {
     return (
         <div className="flex flex-col min-h-full bg-black pt-16 pb-20 w-full">
                 {/* <h1 className="text-white text-center text-6xl font-title mb-8">ATHLETE SEARCH</h1> */}
-                <Filters defaultRostrId={rostrId ? rostrId : undefined} />
-                <div className='px-8'>
-                    <MatchingProfiles />
-                    <PageNumbers />
-                </div>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Filters defaultRostrId={rostrId ? rostrId : undefined} />
+                    <div className='px-8'>
+                        <MatchingProfiles />
+                        <PageNumbers />
+                    </div>
+                </Suspense>
         </div>
     );
 }
