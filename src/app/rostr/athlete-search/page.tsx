d1990ceb0cbf8534e31d131057925ed441532677
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2024-10-06 18:42:51 -0500

    styling

--- src/app/rostr/athlete-search/page.tsx
+++ src/app/rostr/athlete-search/page.tsx
@@ -19,16 +19,16 @@ function Content() {
     return (
         <Container className='w-full h-full flex flex-col'>
             <ResumeModal />
-            <div className='flex flex-row mb-4 justify-between md:justify-start gap-16'>
-                <Link href='/rostr' className='text-slate-300 hover:text-slate-400 inline-flex gap-x-2 items-end'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mb-1">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+            <div className='flex flex-row mb-4 justify-between text-md md:justify-start gap-16'>
+                <Link href='/rostr' className='text-slate-300 hover:text-slate-400 inline-flex gap-x-2 items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                     </svg>
                     Back to Rostrs
                 </Link>
                 {Array.isArray(rostr?.recruits) && rostr.recruits.length > 0 && (
-                <Link href={`/rostr/${rostr.id}`} className='text-slate-300 hover:text-slate-400 inline-flex gap-x-2 items-end'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 mb-1">
+                <Link href={`/rostr/${rostr.id}`} className='text-slate-300 hover:text-slate-400 inline-flex gap-x-2 items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
                     Export Rostr
--- src/components/rostr/export-rostr/ManageRostr.tsx
+++ src/components/rostr/export-rostr/ManageRostr.tsx
@@ -97,15 +97,15 @@ export default function ManageRostr({rostrId, adminEmail}: {rostrId: string, adm
     }
 
     return (<div className="flex flex-col">
-            <div className='flex flex-row justify-start items-end space-x-16'>
-                <Link href="/rostr" className="text-gray-400 inline-flex items-center justify-start hover:text-white text-xl mb-8">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <div className='flex flex-row justify-start text-md items-end space-x-16'>
+                <Link href="/rostr" className="inline-flex items-center justify-start text-slate-300 hover:text-slate-400 mb-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                     </svg>
                     <span className='ml-2'>Back to Rostrs</span>
                 </Link>
-                <Link href={`/rostr/athlete-search${rostr ? `?rostrId=${rostr.id}` : null}`} className="text-gray-400 inline-flex items-center justify-start hover:text-white text-xl mb-8">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                <Link href={`/rostr/athlete-search${rostr ? `?rostrId=${rostr.id}` : null}`} className="inline-flex items-center justify-start text-slate-300 hover:text-slate-400 mb-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
                         <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
                     </svg>
                     <span className='ml-2'>Add More Candidates</span>
